5109e7aac5d923ec7a13a9fcc2965dfeedd1f437
Monika Mráziková <monika.monika.mrazikova@gmail.com>
2022-04-12 09:56:07 +0200

    minimal database created

---
+++ server/db/migrations/20220412071023-create-user.js
@@ -0,0 +1,80 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: {
+            msg: "Email address is invalid",
+          },
+        },
+      },
+      password: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+        validate: {
+          len: {
+            args: [8],
+            msg: "Minimum password length is 8 characters",
+          },
+          notEmpty: {
+            args: [true],
+            msg: "Please enter a password",
+          },
+          notNull: {
+            args: [true],
+            msg: "Please enter a password",
+          },
+        },
+      },
+      first_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      last_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      date_birth: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      role: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      speciality: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
---
+++ server/db/models/user.js
@@ -0,0 +1,77 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({ Post }) {
+      // define association here
+      this.hasMany(Post, { foreignKey: "user_id" })
+    }
+  }
+  User.init({
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: {
+          msg: "Email address is invalid",
+        },
+      },
+    },
+    password: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: {
+          args: [8],
+          msg: "Minimum password length is 8 characters",
+        },
+        notEmpty: {
+          args: [true],
+          msg: "Please enter a password",
+        },
+        notNull: {
+          args: [true],
+          msg: "Please enter a password",
+        },
+      },
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    date_birth: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    speciality: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
