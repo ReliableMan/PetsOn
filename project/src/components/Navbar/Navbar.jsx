76e162313f149e3bff91ab117a93ec6ba2099c40
Evgy <evgybarabanova@gmail.com>
2022-04-12 14:01:47 +0300

    pic

--- project/src/components/Navbar/Navbar.jsx
+++ project/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 // { useEffect }
 import { Link } from 'react-router-dom';
 // import { useDispatch, useSelector } from "react-redux";
@@ -8,30 +9,33 @@ export default function Navbar() {
     <>
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
-    <a className="navbar-brand" to="/">картинка петсОн</a>
+    <a className="navbar-brand" to="/"><img src="images/PetsOn.png" alt="logo" style={{width: '5rem'}}/></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/cat">Статьи</Link>
+          <Link className="nav-link active" aria-current="page" to="/post">Статьи</Link>
         </li>
       </ul>
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/cat">Услуги</Link>
+          <Link className="nav-link active" aria-current="page" to="/services">Услуги</Link>
         </li>
-      
       </ul>
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/cat">О нас</Link>
+          <Link className="nav-link active" aria-current="page" to="/about">О нас</Link>
+        </li>
+      </ul>
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+         <Link className="nav-link" to="/sign"><img src="images/paw.png" alt="logo" style={{width: '4rem'}}/></Link>
         </li>
       </ul>
     </div>
   </div>
-  <Link className="nav-link" to="sign">Лапка</Link>
 </nav>
     </>
   )
