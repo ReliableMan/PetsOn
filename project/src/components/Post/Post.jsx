d6f949c98cc14cbc37285a6730df90ca87aa67ab
Evgy <evgybarabanova@gmail.com>
2022-04-12 18:16:51 +0300

    pictures

--- project/src/components/Post/Post.jsx
+++ project/src/components/Post/Post.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './post.css'
 
 export default function Post() {
   return (
-    <>
-    <div>Post</div>
-    </>
+    <div className='container'>
+    <div className='text-title'>О ком будем читать?</div>
+    <div className='container-link'>
+    <Link className="cat" to="/cat"><img src="images/cat.png" alt="logoCat" style={{width: '5rem'}}/></Link>
+    <Link className="cat" to="/dog"><img src="images/dog.png" alt="logoCat" style={{width: '5rem'}}/></Link>
+    </div>
+
+    <div className='container-post'>
+      <Link className='post1' to="/post1">Название статьи</Link>
+      <Link className='post2' to="/post2">Название статьи</Link>
+      <Link className='post3' to="/post3">Название статьи</Link>
+    </div>
+    
+    </div>
   )
 }
