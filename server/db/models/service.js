fbb023f38bbf916d23bda4092de5104c9b696657
Monika Mráziková <monika.monika.mrazikova@gmail.com>
2022-04-12 15:29:07 +0200

    services components added, service model added

---
+++ server/db/models/service.js
@@ -0,0 +1,41 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Service extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({ User }) {
+      // define association here
+      this.belongsTo(User, { foreignKey: 'user_id' });
+    }
+  }
+  Service.init({
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Users',
+        key: 'id'
+      }
+    },
+  }, {
+    sequelize,
+    modelName: 'Service',
+  });
+  return Service;
+};
