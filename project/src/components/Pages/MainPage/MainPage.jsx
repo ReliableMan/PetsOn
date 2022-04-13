4d95a73d211fb258d74d7163026d8c0028972f90
Evgy <evgybarabanova@gmail.com>
2022-04-13 11:52:09 +0300

    main

--- project/src/components/Pages/MainPage/MainPage.jsx
+++ project/src/components/Pages/MainPage/MainPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./mainPage.css";
+import { Link } from 'react-router-dom';
 
 export default function MainPage() {
   return (
@@ -11,13 +12,16 @@ export default function MainPage() {
       <a className="catdog">
         <img src="images/main.png" alt="logoMain" style={{ width: "12rem" }} />
       </a>
-      <div className="container_link">
         <p>Наши услуги</p>
-        <div className="card">
-        
-        </div>
+      <div className="container_link">
+        <Link className="card" to="/walking"><img src="images/walk.png" alt="logoCard"/></Link> 
+        <Link className="card" to="/vet"><img src="images/vet.png" alt="logoCard"/></Link>
+        <Link className="card" to="/groom"><img src="images/groom.png" alt="logoCard"/></Link>
+        <Link className="card" to="/other"><img src="images/other.png" alt="logoCard"/></Link>
       </div>
-      <button>Найти услуги</button>
+      <button >Найти услуги</button>
+
+      <div className="container_friend"></div>
     </>
   );
 }
