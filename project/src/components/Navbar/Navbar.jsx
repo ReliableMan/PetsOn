6554b8b2a4a9aa4a935023dd453065ce64a0203e
maria.zheleznyakova@gmail.com <mariazheleznyakova@MacBook-Pro.local>
2022-04-14 16:58:00 +0300

    главная старница для всех размеров + навбар

--- project/src/components/Navbar/Navbar.jsx
+++ project/src/components/Navbar/Navbar.jsx
@@ -5,32 +5,32 @@ import "./navbar.css"
 export default function Navbar() {
   return (
     <div className='container'>
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg">
   <div className="container-fluid">
-    <Link className="navbar-brand" to="/"><img src="images/PetsOn.png" alt="logoPet" style={{width: '5rem'}}/></Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <Link className="navbar-brand" to="/"><img src="images/PetsOn.png" alt="logoPet" style={{width: '7rem'}}/></Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/post">Статьи</Link>
+          <Link className="nav-link active" aria-current="page" to="/post">СТАТЬИ</Link>
         </li>
 
       </ul>
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/services">Услуги</Link>
+          <Link className="nav-link active" aria-current="page" to="/services">УСЛУГИ</Link>
         </li>
       </ul>
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/about">О нас</Link>
+          <Link className="nav-link active" aria-current="page" to="/about">О НАС</Link>
         </li>
       </ul>
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/profile">Личный кабинет</Link>
+          <Link className="nav-link active" aria-current="page" to="/profile">ЛИЧНЫЙ КАБИНЕТ</Link>
         </li>
       </ul>
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
