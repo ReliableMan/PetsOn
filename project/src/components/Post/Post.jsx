7dd57891dc96b9e918e034976847a1e7f09415b7
Evgy <evgybarabanova@gmail.com>
2022-04-14 17:55:14 +0300

    ax

--- project/src/components/Post/Post.jsx
+++ project/src/components/Post/Post.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 import "./post.css";
 
 export default function Post() {
+  const [post, setPost] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3903/pet')
+    .then((allPostsFromServer)=>{
+     // console.log(allPostsFromServer.data[0].title);
+
+     const arr = [];
+      allPostsFromServer.data.forEach(i => 
+       arr.push(i.title))
+      //console.log('7777777', allPostsFromServer.data);
+     setPost(allPostsFromServer.data)
+    })
+  },[]);
+
+//console.log('111', post);
+
   return (
     <div className="container-all">
       <div className="text-title">О ком будем читать?</div>
@@ -16,16 +34,22 @@ export default function Post() {
       </div>
 
       <div className="container-post">
-        <Link className="post1" to="/post1">
-          Название статьи
-        </Link>
-        <Link className="post2" to="/post2">
-          Название статьи
-        </Link>
-        <Link className="post3" to="/post3">
-          Название статьи
-        </Link>
-      </div>
+        {post.map(el=>  
+           (<Link className="post1" key={el.id} to='/post/`${id}`'>
+          {el.title}
+        </Link>))}
+
+
+      {/* //   <Link className="post1" to="/post1">
+      //     {post[0]}
+      //   </Link>
+      //   <Link className="post2" to="/post2">
+      //     Название статьи
+      //   </Link>
+      //   <Link className="post3" to="/post3">
+      //     Название статьи
+      //   </Link> */}
+       </div>
     </div>
   );
 }
