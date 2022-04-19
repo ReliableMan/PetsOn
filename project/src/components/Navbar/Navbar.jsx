e7e2085c3f2920c35551b3eccb55aeccd56dd5e9
Evgy <evgybarabanova@gmail.com>
2022-04-19 08:11:10 +0300

    nav bar remove

--- project/src/components/Navbar/Navbar.jsx
+++ project/src/components/Navbar/Navbar.jsx
@@ -61,13 +61,6 @@ export default function Navbar() {
             </ul>
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link" aria-current="page" to="/about">
-                  О НАС
-                </Link>
-              </li>
-            </ul>
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
                 <Link
                   className="nav-link"
                   aria-current="page"
